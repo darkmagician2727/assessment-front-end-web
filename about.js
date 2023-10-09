4f1eeb31dd141cbef288b05831e016ac941c3a25
Thomas Albrecht <darkmagician2727@gmail.com>
2023-10-09 14:59:43 -0600

    whoops

--- about.js
+++ about.js
@@ -4,11 +4,16 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	alert('form submitted');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let img = document.querySelector('#logo_image');
+
+img.addEventListener('mouseover', function() {
+    alert("You're doing great! Thanks for visiting our website.");
+});
